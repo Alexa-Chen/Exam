d42d9da4b4f95a86d251cb504256631de608ed67
chenmo <wwwchenyuhan@qq.com>
2015-08-29 15:35:14 +0200

    Initial commit

---
+++ app/src/main/java/chenmo/neusoft/com/exam/Question.java
@@ -0,0 +1,20 @@
+package chenmo.neusoft.com.exam;
+
+/**
+ * Created by chenmo on 2015/8/29.
+ */
+public class Question   {
+    public  String question;
+    public  String answerA;
+    public  String answerB;
+    public String  answerC;
+    public  String answerD;
+    public  int  answer;
+    public  String explaination;
+    public  int ID;
+
+    public  int selectionAnswer;
+
+
+
+}
